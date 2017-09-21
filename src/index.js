1a860de76fb00dd262d03af2495fbc28d9ebc39a
Steve Pervikov <stepan.pervikov@sap.com>
2017-09-22 00:25:39 +0300

    upload file doesnt work correctly

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//ReactDOM.render(<App txt="My own text goes here"/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
